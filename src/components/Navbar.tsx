5d4fa1c6eafbe7ab2f5fd440eae825d6a4369344
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2025-11-21 00:16:46 +0700

    edit: update navbar menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,10 @@
 import { Link, useLocation } from "react-router-dom";
 import { Layout, Menu } from "antd";
-import { MenuOutlined } from "@ant-design/icons";
-import { useState } from "react";
 
 const { Header } = Layout;
 
 export default function Navbar() {
   const location = useLocation();
-  const [collapsed, setCollapsed] = useState(true);
 
   const items = [
     { key: "/", label: <Link to="/">Todos</Link> },
@@ -18,50 +15,36 @@ export default function Navbar() {
     <Header
       style={{
         background: "#fff",
-        padding: "0 20px",
+        padding: "0 24px",
         display: "flex",
         alignItems: "center",
         justifyContent: "space-between",
       }}
     >
-      {/* Logo */}
-      <div style={{ fontSize: 20, fontWeight: 600 }}>
+      {/* Brand Title */}
+      <div
+        style={{
+          fontSize: 20,
+          fontWeight: 600,
+          whiteSpace: "nowrap",      // <-- penting!
+          marginRight: 24,
+        }}
+      >
         Industrix Todo
       </div>
 
-      {/* Desktop Menu */}
+      {/* Menu */}
       <Menu
         mode="horizontal"
         selectedKeys={[location.pathname]}
         items={items}
+        overflowedIndicator={null}   // <-- mencegah menu ...
         style={{
-          borderBottom: "none",
-          display: window.innerWidth < 600 ? "none" : "block",
+          flex: 1,
+          justifyContent: "flex-end",
+          minWidth: 0,               // <-- FIX RESPONSIVE
         }}
       />
-
-      {/* Mobile Menu Button */}
-      <MenuOutlined
-        className="mobile-menu-toggle"
-        style={{ fontSize: 22, cursor: "pointer", display: window.innerWidth < 600 ? "block" : "none" }}
-        onClick={() => setCollapsed((c) => !c)}
-      />
-
-      {/* Mobile Dropdown */}
-      {!collapsed && window.innerWidth < 600 && (
-        <Menu
-          mode="vertical"
-          items={items}
-          selectedKeys={[location.pathname]}
-          style={{
-            position: "absolute",
-            top: 64,
-            right: 0,
-            width: "100%",
-            borderTop: "1px solid #eee",
-          }}
-        />
-      )}
     </Header>
   );
 }
